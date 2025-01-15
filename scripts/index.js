369d0e13825237a43ecf157a918630c9b6e68331
Ervis Trupja <ervis@bettertrucks.com>
2025-01-15 13:32:48 +0100

    added jquery - google translate

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,38 @@ $appH1.on("click", () => {
 $appP.on("click", () => {
   $dateTimeNow.html("P u klikua: " + new Date());
 });
+
+$("#translateBtn").on("click", () => {
+  let fromLanguage = $("#fromLanguage").val();
+  console.log("fromLanguage - ", fromLanguage);
+  let toLanguage = $("#toLanguage").val();
+  console.log("toLanguage - ", toLanguage);
+
+  let textToBeTranslated = $("#translateInput").val();
+
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://google-translate113.p.rapidapi.com/api/v1/translator/html",
+    method: "POST",
+    headers: {
+      "x-rapidapi-key": "6605cb8369msh171a9fccea13ac0p174da2jsn4ad279364885",
+      "x-rapidapi-host": "google-translate113.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+    processData: false,
+    data:
+      '{"from":"' +
+      fromLanguage +
+      '","to":"' +
+      toLanguage +
+      '","html":"<div>' +
+      textToBeTranslated +
+      '</div>"}',
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    $("#translationPlaceholder").html(response.trans);
+  });
+});
